78be2c3e58ace471d24f67cfd6a47aade0ecc77d
Stavros Thalassinos <40219645+stavthal@users.noreply.github.com>
2022-05-23 02:19:11 +0300

    Update on index.js
    Now it correctly shows the supposed image

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-
-
-// const image1 = document.getElementsByClassName("img1");
-// const image2 = document.getElementsByClassName("img2");
-
-
-// console.log(image1);
-// console.log(image2);
-
-
-// image1.src =`images/dice${randomNumber1}.png`;
-// image2.src =`images/dice${randomNumber2}.png`;
+if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🏴";
+} else if (randomNumber2 < randomNumber1) {
+    document.querySelector("h1").innerHTML = "🏴 Player 1 Wins!";
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "It's a Draw!";
+};
 
 
 document.querySelector(".img1").src=`images/dice${randomNumber1}.png`;
-document.querySelector(".img1").src=`images/dice${randomNumber2}.png`;
+document.querySelector(".img2").src=`images/dice${randomNumber2}.png`;
 
 
-if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🏴";
-} else if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🏴 Player 1 Wins!";
-} else {
-    document.querySelector("h1").innerHTML = "It's a Draw!";
-}
